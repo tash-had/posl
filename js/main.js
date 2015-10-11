1f95a38d3f9addc37d7498a04ea4563c9fc1a482
tash-had <nanylagoon@gmail.com>
2015-10-10 23:28:42 -0400

    miscellaneous

--- js/main.js
+++ js/main.js
@@ -200,10 +200,11 @@ function randomArticle() {
 
 function aboutClick() {
     var info = "The purpose of Posl is to provide you with uplifting news stories. In contrast to what we generally see in everyday news, Posl aims to spread the good things happening in the world. Posl fetches news articles from the following sources:";
-    var sourceOne = "<br><a href='http://www.reddit.com/r/upliftingnews/' target=_blank>Reddit - UpliftingNews</a>";
+    var sourceOne = "<br><br><a href='http://www.reddit.com/r/upliftingnews/' target=_blank>Reddit - UpliftingNews</a>";
     var sourceTwo = "<br><a href='http://www.webofgoodnews.com/' target=_blank>Tumblr - Web Of Good News</a>";
     var sourceThree = "<br><a href='http://www.good-news-network.tumblr.com/' target=_blank>Tumblr - Good News Network</a>";
-    var openSource = "<br><br>Also, this project is opensource and <a href='http://github.com/tash-had/posl' target=_blank>can be found on my GitHub</a>";
+    var openSource = "<br><br>This project is opensource and <a href='http://github.com/tash-had/posl' target=_blank>can be found on my GitHub</a>";
 
+    alertify.okBtn("Okay");
     alertify.alert(info + sourceOne + sourceTwo + sourceThree + openSource);
 }
