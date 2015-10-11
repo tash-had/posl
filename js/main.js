4ec63f5d8ade900dd2fcb22788682a809b848df1
tash-had <nanylagoon@gmail.com>
2015-10-10 23:47:10 -0400

    miscellaneous

--- js/main.js
+++ js/main.js
@@ -146,9 +146,9 @@ tumblrJsonCallback = function(data) {
                 }
             }
         });
-    } catch (err) {
-        console.log("err");
-    }
+} catch (err) {
+    console.log("err");
+}
 }
 
 
@@ -199,12 +199,12 @@ function randomArticle() {
 }
 
 function aboutClick() {
+    var openSource = "Posl is opensource and <a href='http://github.com/tash-had/posl' target=_blank>can be found on my GitHub. </a>";
     var info = "The purpose of Posl is to provide you with uplifting news stories. In contrast to what we generally see in everyday news, Posl aims to spread the good things happening in the world. Posl fetches news articles from the following sources:";
-    var sourceOne = "<br><br><a href='http://www.reddit.com/r/upliftingnews/' target=_blank>Reddit - UpliftingNews</a>";
-    var sourceTwo = "<br><a href='http://www.webofgoodnews.com/' target=_blank>Tumblr - Web Of Good News</a>";
-    var sourceThree = "<br><a href='http://www.good-news-network.tumblr.com/' target=_blank>Tumblr - Good News Network</a>";
-    var openSource = "<br><br>This project is opensource and <a href='http://github.com/tash-had/posl' target=_blank>can be found on my GitHub</a>";
+    var sourceOne = "<br><li><a href='http://www.reddit.com/r/upliftingnews/' target=_blank>Reddit - /r/UpliftingNews</a></li>";
+    var sourceTwo = "<li><a href='http://www.webofgoodnews.com/' target=_blank>Tumblr - Web Of Good News</a></li>";
+    var sourceThree = "<li><a href='http://www.good-news-network.tumblr.com/' target=_blank>Tumblr - Good News Network</a></li>";
 
     alertify.okBtn("Okay");
-    alertify.alert(info + sourceOne + sourceTwo + sourceThree + openSource);
+    alertify.alert(openSource + info + sourceOne + sourceTwo + sourceThree);
 }
